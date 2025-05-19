763e6e722edbe82928362461a074b344278a6e6b
Hsuan93 <hsuan9301@gmail.com>
2025-05-19 23:42:07 +0800

    Add branch dev

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from hello import hello
-from datetime import datetime
+from datetime import datetime  #from dev分支的註解
 
 if __name__ == "__main__":
     name = "Your Name" # 自行替換裡面的名字
     hello(name)
 
-    now = datetime.now()
+    now = datetime.now()  #加入時間顯示
     time_str = now.strftime("%Y-%m-%d %H:%M:%S")
     print(f"\n現在時間是 {time_str}")
