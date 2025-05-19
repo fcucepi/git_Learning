c934833a22ac676187060a644ac3e908877ad3a2
Hsuan93 <hsuan9301@gmail.com>
2025-05-19 23:10:36 +0800

    Add datetime

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from hello import hello
+from datetime import datetime
 
 if __name__ == "__main__":
     name = "Your Name" # 自行替換裡面的名字
     hello(name)
-    # 123
+
+    now = datetime.now()
+    time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"\n現在時間是 {time_str}")
