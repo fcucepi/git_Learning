a35cbe4b47ef74e3a8f66eedd2464e0ddde7f2f3
fcucepi <fcucepi@gmail.com>
2025-06-16 13:12:36 +0800

    Add remove old containers and change show status
    新增在預設會先將舊的容器先 down 後再看有沒有要修剪的
    container，對於顯示狀態的部分，不直接使用 echo 將 ip
    顯示出來，改成呼叫 docker ps -a 顯示結果

--- my-private-cloud/deploy.sh
+++ my-private-cloud/deploy.sh
@@ -3,9 +3,18 @@ echo "creating folders..."
 mkdir -p /mnt/data/photos/uploads
 mkdir -p /mnt/data/config
 
+echo ""
+echo ""
+echo "rm old docker containers..."
+docker compose down
+docker container prune
+
+echo ""
+echo ""
 echo "starting docker containers..."
 docker compose up -d
 
-echo "deployment completed"
-echo "- Immich is available at: http://localhost:2299"
-echo "- FileBrowser is available at: http://localhost:8080"
+echo ""
+echo ""
+echo "This is your docker status"
+docker ps -a
